3e3d5657762651c0fa6bc8796a3757e1e868a3a0
hursty1 <danhursty@gmail.com>
2020-09-21 08:10:03 +0000

    updated snk

--- snk.py
+++ snk.py
@@ -89,6 +89,13 @@ def random_move(snk, body, size):
       
     return move
 
+def distance(v1, v2):
+  """
+  Distance between two vectors
+  """
+  return math.sqrt(math.pow(v1[0]-v2[0], 2) + math.pow(v1[1]-v2[1], 2))
+
+
 def simple(snk, body, size, food):
   """
   Simple algorithm it should 
@@ -105,4 +112,87 @@ def simple(snk, body, size, food):
   closest = f_len.index(min(f_len))
   closest_food = food[closest]
 
-  
+
+class Node():
+  def __init__(self, parent=None, position=None):
+    self.parent = parent
+    self.position = position
+
+    self.g = 0
+    self.h = 0
+    self.f = 0
+  def __eq__(self, other):
+    return self.position == other.position
+
+def astar(board, start, end):
+  """
+  Returns a list of commands
+  """
+  start_node = Node(None, start)
+  start_node.g = start_node.h = start_node.f = 0
+  end_node = Node(None, end)
+
+  #initalize both open and closed list
+  open_list = []
+  closed_list = []
+
+  #add the start Node
+  open_list.append(start_node)
+
+  #loop until you find the end
+  while len(open_list) > 0:
+    current_node = open_list[0]
+    current_index = 0
+    for index, item in enumerate(open_list):
+      if item.f < current_node.f:
+        current_node = item
+        current_index = index
+    
+    open_list.pop(current_index)
+    closed_list.append(current_node)
+
+    if current_node == end_node:
+      path = []
+      current = current_node
+      while current is not None:
+        path.append(current.position)
+        current = current.parent
+      return path[::-1] # Return reversed path
+    
+    children = []
+    for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: #adjcent squares
+      node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+      
+      if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
